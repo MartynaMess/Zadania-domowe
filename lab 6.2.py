a3844ca97a22d3f204dbcd6ea01a4dc16469a5cb
MartynaMess <82751369+MartynaMess@users.noreply.github.com>
2021-05-16 10:51:48 +0200

    Add files via upload

---
+++ lab 6.2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 10:37:20 2021
+
+@author: marty
+"""
+
+def funkcja():
+    x = int(input("x = "))
+    y = int(input("y = "))
+    z = int(input("z = "))
+    return x+y+z
+
+print (funkcja())
+print (funkcja())
+print (funkcja())
